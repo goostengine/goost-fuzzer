e53e96c58039de349ace1417a52a07732534726e
Andrii Doroshenko (Xrayez) <xrayez@gmail.com>
2021-08-05 13:52:39 +0300

    Generate a list of Goost classes to fuzz
    We're not interested in catching bugs which come directly from Godot, this is out of our control.

---
+++ generate_class_list.py
@@ -0,0 +1,21 @@
+import os
+import sys
+
+if not os.path.exists("goost"):
+    print("ERROR: Goost repository is not checked out.")
+    sys.exit(255)
+
+if not os.path.exists("fuzzer"):
+    print("ERROR: Fuzzer repository is not checked out.")
+    sys.exit(255)
+
+sys.path.insert(0, "goost")
+
+from goost import classes
+
+with open("fuzzer/classes.txt", "w") as f:
+    for name in classes:
+        f.write(name + "\n")
+
+sys.path.remove("goost")
+sys.modules.pop("goost")
